a3022de7540a9cc2c6f80c921a6ca96cc7f20c06
Christian Fei <crifei93@gmail.com>
2017-11-12 22:09:05 +0100

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+this project should help to serve this purpose:
+
+- after a webhook has been triggered by github
+- clone the latest commit from this repo
+- run the tests
+- deploy the up project
+
+
+## installation
+
+```
+npm i
+```
+
+## deploy
+
+```
+up
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const {spawn} = require('child_process')
+const express = require('express')
+const app = express()
+
+const {PORT = 3000} = process.env
+
+app.post('/', function (req, res) {
+  console.log('webhook payload', req.body)
+  // const args = req.query.args ? req.query.args.split(',') : []
+  spawnPgm('echo', ['hello'], (code, output) => {
+    console.log('output', output)
+    res.set('Content-Type', 'text/plain')
+    res.send(output)
+  })
+})
+
+function spawnPgm (pgm, args, done) {
+  const run = spawn(pgm, args)
+  let output = ''
+
+  run.stdout.on('data', data => { output += data })
+  run.stderr.on('data', data => { output += data })
+  run.on('close', code => done(code, output))
+}
+
+app.listen(PORT)
