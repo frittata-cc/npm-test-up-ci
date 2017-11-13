14dfb8c186ed684c942bad58a5bbfe2bcb0a720c
Christian Fei <crifei93@gmail.com>
2017-11-13 22:51:15 +0100

    wihtout `

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ app.use(bodyParser.json())
 
 app.post('/', function (req, res) {
   const {head_commit: {id: commit}, repository: {clone_url}} = req.body
-  console.log(`=> clone commit ${commit}`)
+  console.log('=> clone commit', commit)
   console.log('clone_url', clone_url)
-  let url = clone_url.replace('//github', `//${process.env.GITHUB_ACCESS_USERNAME}:${process.env.GITHUB_ACCESS_TOKEN}@github`)
+  let url = clone_url.replace('//github', '//' + process.env.GITHUB_ACCESS_USERNAME + ':' + process.env.GITHUB_ACCESS_TOKEN + '@github')
 
   tasks(url, commit).run()
   .then(() => {
