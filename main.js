47badd9f0f686bd1b52c85be9da1d6f49b20a77d
Christian Fei <crifei93@gmail.com>
2017-11-17 20:08:01 +0100

    just log body

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const tasks = require('./tasks')
+// const tasks = require('./tasks')
 app.use(bodyParser.json())
 
 app.post('/', function (req, res) {
-  const commit = req.body.head_commit.id
-  const cloneUrl = req.body.repository.clone_url
-  console.log('=> clone commit', commit, '@', cloneUrl)
-  let url = cloneUrl.replace('//github', '//' + process.env.GITHUB_ACCESS_USERNAME + ':' + process.env.GITHUB_ACCESS_TOKEN + '@github')
+  console.log(req.body)
+  // const commit = req.body.head_commit.id
+  // const cloneUrl = req.body.repository.clone_url
+  // console.log('=> clone commit', commit, '@', cloneUrl)
+  // let url = cloneUrl.replace('//github', '//' + process.env.GITHUB_ACCESS_USERNAME + ':' + process.env.GITHUB_ACCESS_TOKEN + '@github')
 
-  tasks(url, commit).run()
-  .then(function () {
-    res.send({})
-  }).catch(function (err) {
-    console.error(err)
-  })
+  // tasks(url, commit).run()
+  // .then(function () {
+  //   res.send({})
+  // }).catch(function (err) {
+  //   console.error(err)
+  // })
 })
 
 module.exports = app
